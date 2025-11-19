41142b38bebe2778be383f5b68265c3de36b2885
Krisz <torokkrisztofer46@gmail.com>
2025-11-19 14:15:41 +0100

    feat: add basic web template

---
+++ cukraszda/src/main/java/hu/nje/cukraszda/CukraszdaApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.cukraszda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukraszdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CukraszdaApplication.class, args);
+    }
+
+}
---
+++ cukraszda/src/test/java/hu/nje/cukraszda/CukraszdaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nje.cukraszda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukraszdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
